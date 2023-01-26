4d1f6114abcd9242b9e401dfcdfc9864a33bfe5c
sononeswati21 <101490118+sononeswati21@users.noreply.github.com>
2023-01-26 05:29:54 -0600

    trf demo

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+     home: Material(
+      child: Center(child: Container(child: Text("welcome to flutter demo app"),)),
+     ),
+    );
+  }
+}
